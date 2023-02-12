57526b2fc10802021d219ec20c945e30eb16bb30
saki <16546851+sakiko-2@users.noreply.github.com>
2023-02-12 16:48:29 +1300

    Minimum work hours per week

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -37,7 +37,7 @@ def calculate_demographic_data(print_data=True):
     lower_education_rich = round((n_rich_lower_education / n_lower_education) * 100, 1)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    min_work_hours = df['hours-per-week'].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
     num_min_workers = None
--- test_module.py
+++ test_module.py
@@ -31,10 +31,10 @@ class DemographicAnalyzerTestCase(unittest.TestCase):
         expected = 17.4
         self.assertAlmostEqual(actual, expected, msg="Expected different value for percentage without higher education that earn >50K.")
 
-    # def test_min_work_hours(self):
-    #     actual = self.data['min_work_hours']
-    #     expected = 1
-    #     self.assertAlmostEqual(actual, expected, msg="Expected different value for minimum work hours.")     
+    def test_min_work_hours(self):
+        actual = self.data['min_work_hours']
+        expected = 1
+        self.assertAlmostEqual(actual, expected, msg="Expected different value for minimum work hours.")     
 
     # def test_rich_percentage(self):
     #     actual = self.data['rich_percentage']
